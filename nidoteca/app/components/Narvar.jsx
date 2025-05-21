f25343b76181c8ad430ead97f09d1bab1f120ba7
RAFAELRGR <rafaelronaldog@gmail.com>
2025-05-21 18:36:15 -0500

    Update NarVar

--- nidoteca/app/components/Narvar.jsx
+++ nidoteca/app/components/Narvar.jsx
@@ -163,13 +163,6 @@ export default function Navbar() {
             Requisitos
           </Link>
           <Link
-            href="/costos"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-blue-900 hover:bg-blue-50"
-            onClick={closeMenu}
-          >
-            Costos y Tarifas
-          </Link>
-          <Link
             href="/CommentsSection"
             className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-blue-900 hover:bg-blue-50"
             onClick={closeMenu}
